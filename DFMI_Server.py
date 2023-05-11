a53f20bc2915f28212ec41166365ab9d827a8002
SethJGibson <72580436+SethJGibson@users.noreply.github.com>
2023-05-10 23:13:17 -0400

    Added Shell Functionality

--- DFMI_Server.py
+++ DFMI_Server.py
@@ -19,7 +19,7 @@ def handler(host_ip, host_port):
                 break
 
             response = remote_target.recv(1024).decode()
-            print('[+] Message from client: ' + response)
+            print('[+] Message from client: \n' + response)
 
             if response == 'exit':
                 print('[-] The client has terminated the session.')
